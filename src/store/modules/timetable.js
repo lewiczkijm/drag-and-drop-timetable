6d791e6af458e4bde03f95b4ff8d66c579b4e46a
Michael <lewiczkij.m@yandex.ru>
2020-10-29 10:06:51 +0200

    refactor mutation CREATE_TIMETABLE. Delete deprecated from it

--- src/store/modules/timetable.js
+++ src/store/modules/timetable.js
@@ -61,70 +61,30 @@ const state = ()=>({
 });
 
 const mutations = {
+    // Подготовка списка уроков и вспосогательных параметров, зависимых от них
     CREATE_TIMETABLE(state){
+        const startDay=1; // Начало недели - 0 воскресенье, 1 - понедельник
+        const HOUR0 = 5;          // час начала рабочего дня
 
+        const d = new Date(state.lessons[0].date);
+        const START_DATE = d.getDay() - startDay;
+        let activeDate = new Date(d.getTime() - 86400000 * START_DATE);     // нахождение первого дня недели
 
-        /**
-         * Подготовка структур длнных недели
-         * @param startDate строка даты из входа в формате YYYY-MM-DD
-         * @param startDay Начало недели - 0 воскресенье, 1 - понедельник ...
-         */
-        function createEmptyWeek(startDate,startDay=1) {
-            const HOUR0 = 5;          // час начала рабочего дня
-            const d = new Date(startDate);
-            const START_DATE = d.getDay() - startDay;
-            let activeDate = new Date(d.getTime() - 86400000 * START_DATE);     // нахождение первого дня недели
-
-            // Получение списка дней недели с датами
-            for(let i = 0;i < 7;i ++){
-                state.dates.push(state.weekNames[i] + ", " + (activeDate.getDate() + i))
-            }
-
-            // Рассчет каординат grid в формате x, y, y-end
-            state.lessons.forEach(key=>{
-                const x = (new Date(key.date)).getDate() - activeDate.getDate() + 1;
-                const y = key.start * 2 - HOUR0 * 2 + 1;
-                const yEnd = key.end * 2 - HOUR0 * 2 + 1;
-                key.koordinates = {x:x,y:y,yEnd:yEnd}
-            });
-
-
-
-            // Создание такой структуры timetable не актуально для таблицы, написанной на grid
-            const incrementActiveDate = ()=>activeDate = new Date(activeDate.getTime() + 86400000);
-
-            for (let i=0;i < 7;i ++){
-                const dayTT=[];
-                for (let j = 5; j <= 17; j += 0.5) {
-                    dayTT.push({time: j, lesson: undefined})
-                }
-                state.timetable.push({time:[...dayTT],date:activeDate.getDate()});
-                incrementActiveDate();
-            }
+        // Получение списка дней недели с датами
+        for(let i = 0;i < 7;i ++){
+            state.dates.push(state.weekNames[i] + ", " + (activeDate.getDate() + i))
         }
-        createEmptyWeek(state.lessons[0].date);
 
-        // Список уроков
-        state.lessons.forEach(el=>{
-            const day = new Date(el.date).getDate();
-            const currentDayObject = state.timetable.find(e=>e.date === day);
-
-            let firstFlag = true;
-            let time = el.start;
-
-            // Проход по часам для вставки урока в нужное место
-            for(let i = 0;i < 20; i ++){
-                if(currentDayObject.time[i].time === time && time < el.end){
-                    currentDayObject.time[i].lesson = el;
-                    currentDayObject.time[i].isStart = firstFlag;
-                    firstFlag = false;
-                    time +=0.5
-                }
-            }
+        // Рассчет каординат grid в формате x, y, y-end
+        state.lessons.forEach(key=>{
+            const x = (new Date(key.date)).getDate() - activeDate.getDate() + 1;
+            const y = key.start * 2 - HOUR0 * 2 + 1;
+            const yEnd = key.end * 2 - HOUR0 * 2 + 1;
+            key.koordinates = {x:x,y:y,yEnd:yEnd}
         });
-
-
     },
+
+
     // Это проверочная мутация для тестов - не запускать
     TEST_M(state){
         state.dates=[0,1]
@@ -138,12 +98,14 @@ const actions = {
 
     // Перемещение урока
     move({commit},moveData){
+        // TODO make from stub
         commit;
         moveData
     },
 
     // Изменение времени урока
     resize({commit},moveData){
+        // TODO make from stub
         commit;
         moveData
     }
